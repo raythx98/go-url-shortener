cbadf428c8c9d6fcf81efbc268a5f3d5a8ed6726
Ray Toh <raythx98@gmail.com>
2023-01-26 17:16:56 +0800

    Add gRPC

--- service/handler.go
+++ service/handler.go
@@ -1,12 +1,46 @@
 package service
 
 import (
+	"fmt"
 	"net/http"
 
+	"golang.org/x/net/context"
+
+	"github.com/raythx98/go-url-shortener/pb"
 	"github.com/raythx98/go-url-shortener/service/command"
 	"github.com/raythx98/go-url-shortener/service/invoker"
 	"github.com/raythx98/go-url-shortener/service/receiver"
 )
+
+type Server struct {
+	url_shortener_proto.UnimplementedUrlShortenerServiceServer
+}
+
+func (s *Server) TurnOn(ctx context.Context, request *url_shortener_proto.TurnOnRequest) (*url_shortener_proto.TurnOnResponse, error) {
+	fmt.Println("Turning on...")
+	onCommand := &command.OnCommand{
+		Device: &receiver.Tv{},
+	}
+	onButton := &invoker.Button{
+		Command: onCommand,
+	}
+	onButton.Press()
+	fmt.Println("Turned on...")
+	return &url_shortener_proto.TurnOnResponse{}, nil
+}
+
+func (s *Server) TurnOff(ctx context.Context, request *url_shortener_proto.TurnOffRequest) (*url_shortener_proto.TurnOffResponse, error) {
+	fmt.Println("Turning off...")
+	offCommand := &command.OffCommand{
+		Device: &receiver.Tv{},
+	}
+	offButton := &invoker.Button{
+		Command: offCommand,
+	}
+	offButton.Press()
+	fmt.Println("Turned off...")
+	return &url_shortener_proto.TurnOffResponse{}, nil
+}
 
 func (on OnHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Turning on...\n"))
--- service/service.go
+++ service/service.go
@@ -2,21 +2,19 @@ package service
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"net/http"
 
 	"google.golang.org/grpc"
+
+	pb "github.com/raythx98/go-url-shortener/pb"
 )
 
 func AddHandlers() {
-	muxHttp := http.NewServeMux()
-	muxGrpc := http.NewServeMux()
-
 	on := OnHandler{}
 	off := OffHandler{}
-
-	grpcServer := grpc.NewServer()
-	fmt.Println(grpcServer)
-
+	muxHttp := http.NewServeMux()
 	// Use the mux.Handle() function to register this with our new servemux,
 	// so it acts as the customhandler for all incoming requests with the URL path /test.
 	muxHttp.Handle("/on", on)
@@ -24,9 +22,27 @@ func AddHandlers() {
 
 	fmt.Println("Listening...")
 
+	// can listen from any IP Addressl
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", 9282))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+
+	pb.RegisterUrlShortenerServiceServer(grpcServer, &Server{})
+	go func() {
+		err := grpcServer.Serve(lis)
+		if err != nil {
+			log.Fatalf("failed to listen to gRPC port 9282: %v", err)
+		}
+	}()
+
 	// Then we create a new server and start listening for incoming requests
 	// with the http.ListenAndServe() function, passing in our servemux for it to
 	// match requests against as the second parameter.
-	go http.ListenAndServe(":9281", muxHttp)
-	http.ListenAndServe(":9282", muxGrpc)
+	err = http.ListenAndServe(":9281", muxHttp)
+	if err != nil {
+		log.Fatalf("failed to listen to HTTP port 9281: %v", err)
+	}
 }
